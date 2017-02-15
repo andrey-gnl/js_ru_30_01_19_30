3c7b589a7b3178e2332bae13be62f2a20aa6e5e9
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2017-02-15 21:01:53 +0200

    filter by select done

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,21 +1,45 @@
 import React, {Component, PropTypes} from 'react'
 import Article from './Article/index'
+import store from '../store'
 import accordion from '../decorators/accordion'
 
 class ArticleList extends Component {
     render() {
-        const {articles, toggleOpenItem, isOpenItem} = this.props
-        const articleElements = articles.map((article) => <li key={article.id}>
+        const {articles, toggleOpenItem, isOpenItem} = this.props;
+
+        return (
+            <ul>
+                {this.getArticles()}
+            </ul>
+        )
+    }
+
+    getArticles = () => {
+
+		const {articles, toggleOpenItem, isOpenItem} = this.props;
+
+		const filterSelect = store.getState().filterSelect.map((filter) => {
+			return filter.value;
+		});
+		
+		let filteredArticles = articles;
+
+		if (filterSelect.length) {
+
+			 filteredArticles = articles.filter((article) => {
+				return filterSelect.includes(article.id);
+			});
+
+        }
+
+		const articleElements = filteredArticles.map((article) => <li key={article.id}>
             <Article
                 article={article}
                 isOpen={isOpenItem(article.id)}
                 toggleOpen={toggleOpenItem(article.id)}/>
-        </li>)
-        return (
-            <ul>
-                {articleElements}
-            </ul>
-        )
+        </li>);
+
+		return articleElements;
     }
 }
 export default accordion(ArticleList)
