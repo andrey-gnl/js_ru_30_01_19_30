873381b722014411f35ba4efc9fcfe448fa4a91c
Andrey Gnilitskiy <a.gnilitskiy@gmail.com>
2017-02-17 17:04:42 +0200

    hw4 fixes

--- src/components/ArticleList.js
+++ src/components/ArticleList.js
@@ -1,39 +1,89 @@
 import React, {Component, PropTypes} from 'react'
 import Article from './Article/index'
-import store from '../store'
+import {connect} from 'react-redux'
 import accordion from '../decorators/accordion'
 
 class ArticleList extends Component {
 
     render() {
-        const {articles, toggleOpenItem, isOpenItem} = this.props;
-
+        const {articles, toggleOpenItem, isOpenItem, filteredArticles} = this.props;
+		const articleElements = filteredArticles.map((article) => <li key={article.id}>
+			<Article
+				article={article}
+				isOpen={isOpenItem(article.id)}
+				toggleOpen={toggleOpenItem(article.id)}/>
+		</li>);
         return (
             <ul>
-                {this.getArticles()}
+				{articleElements}
             </ul>
         )
     }
 
-    getArticles = () => {
+    // getArticles = () => {
+	//
+		// const {articles, toggleOpenItem, isOpenItem, selection, dates} = this.props;
+	//
+		// const filterSelect = selection.map((filter) => {
+		// 	return filter.value;
+		// });
+	//
+		// const filterDate = dates;
+		//
+		// let filteredArticles = articles;
+	//
+		// //filter from data range
+		// if(filterDate.from) {
+		// 	filteredArticles = filteredArticles.filter((article) => {
+		// 	    return Date.parse(article.date) > Date.parse(filterDate.from)
+    //         })
+    //     }
+    //     //filter to data range
+		// if(filterDate.to) {
+		// 	filteredArticles = filteredArticles.filter((article) => {
+		// 		return Date.parse(article.date) < Date.parse(filterDate.to)
+		// 	})
+		// }
+	//
+		// //filter by select
+		// if (filterSelect.length) {
+	//
+		// 	filteredArticles = filteredArticles.filter((article) => {
+		// 		return filterSelect.includes(article.id);
+		// 	});
+	//
+		// }
+	//
+		// const articleElements = filteredArticles.map((article) => <li key={article.id}>
+    //         <Article
+    //             article={article}
+    //             isOpen={isOpenItem(article.id)}
+    //             toggleOpen={toggleOpenItem(article.id)}/>
+    //     </li>);
+		//
+		// return articleElements;
+	//
+    // }
+}
+export default connect((state) => {
 
-		const {articles, toggleOpenItem, isOpenItem} = this.props;
+		const {filterSelect, filterDates, articles} = state;
 
-		const filterSelect = store.getState().filterSelect.map((filter) => {
+		const selection = filterSelect.map((filter) => {
 			return filter.value;
 		});
 
-		const filterDate = store.getState().filterDates;
-		
+		const filterDate = filterDates;
+
 		let filteredArticles = articles;
 
 		//filter from data range
 		if(filterDate.from) {
 			filteredArticles = filteredArticles.filter((article) => {
-			    return Date.parse(article.date) > Date.parse(filterDate.from)
-            })
-        }
-        //filter to data range
+				return Date.parse(article.date) > Date.parse(filterDate.from)
+			})
+		}
+		//filter to data range
 		if(filterDate.to) {
 			filteredArticles = filteredArticles.filter((article) => {
 				return Date.parse(article.date) < Date.parse(filterDate.to)
@@ -41,26 +91,17 @@ class ArticleList extends Component {
 		}
 
 		//filter by select
-		if (filterSelect.length) {
+		if (selection.length) {
 
 			filteredArticles = filteredArticles.filter((article) => {
-				return filterSelect.includes(article.id);
+				return selection.includes(article.id);
 			});
 
 		}
 
-		const articleElements = filteredArticles.map((article) => <li key={article.id}>
-            <Article
-                article={article}
-                isOpen={isOpenItem(article.id)}
-                toggleOpen={toggleOpenItem(article.id)}/>
-        </li>);
-		
-		return articleElements;
+		return {filteredArticles};
 
-    }
-}
-export default accordion(ArticleList)
+})(accordion(ArticleList))
 
 ArticleList.propTypes = {
     articles: PropTypes.array.isRequired,
